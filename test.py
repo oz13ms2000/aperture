ef9e1023c0f1687e759e09397cf965f17849d0cc
Xing <oz13ms2000@yahoo.com>
2017-10-15 15:55:44 -0700

    trying random stuff

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from pandas_datareader import data
 import pandas as pd
+import stockstats as ss
 
 
 # Define the instruments to download. We would like to see Apple, Microsoft and the S&P500 index.
@@ -9,8 +10,8 @@ tickers = ['AAPL', 'MSFT', 'SPY']
 data_source = 'google'
 
 # We would like all available data from 01/01/2000 until 12/31/2016.
-start_date = '2010-01-01'
-end_date = '2016-12-31'
+start_date = '2017-10-01'
+end_date = '2017-10-10'
 
 # User pandas_reader.data.DataReader to load the desired data. As simple as that.
 panel_data = data.DataReader(tickers, data_source, start_date, end_date)
@@ -18,14 +19,18 @@ panel_data = data.DataReader(tickers, data_source, start_date, end_date)
 # Getting just the adjusted closing prices. This will return a Pandas DataFrame
 # The index in this DataFrame is the major index of the panel_data.
 close = panel_data.ix['Close']
-
+list_panel = list(panel_data)
 # Getting all weekdays between 01/01/2000 and 12/31/2016
 all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
 
 # How do we align the existing prices in adj_close with our new set of dates?
 # All we need to do is reindex close using all_weekdays as the new index
 close = close.reindex(all_weekdays)
+stock = ss.StockDataFrame.retype(panel_data)
 
-close.head(10)
+#close.head(1)
 
 #print(close)
+print(stock)
+#stock.get('AAPL')
+# print(list_panel)
